aaa1b70cc27bb7e4c583345a72c411f4659fb276
AppMath20 <56472870+AppMath20@users.noreply.github.com>
2021-03-31 00:01:04 +0300

    Create main.py

---
+++ Test2/main.py
@@ -0,0 +1,18 @@
+import matplotlib.pyplot as plt
+from matplotlib import gridspec
+import seaborn as sns
+import pandas as pd
+from tqdm.notebook import tqdm
+
+from scipy.special import softmax
+from scipy.spatial.distance import cdist
+import numpy as np
+import torch
+
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import classification_report
+from sklearn.model_selection import KFold, ParameterGrid
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+
+from mlxtend.plotting import plot_decision_regions
